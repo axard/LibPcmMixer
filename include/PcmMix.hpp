f49b180656d73d2ccaff8af398fadad2b933cbf7
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2019-06-01 01:53:22 +0300

    Добавил интерфейс функций смешивания чанков и семплов

---
+++ include/PcmMix.hpp
@@ -0,0 +1,16 @@
+#ifndef PCMMIX_H
+#define PCMMIX_H
+
+#include <tuple>
+#include <Pcm.hpp>
+#include <PcmChunk.hpp>
+
+namespace Pcm {
+
+Chunk Mix(const Chunk& a, const Chunk& b);
+
+int16_t Mix(int16_t a, int16_t b);
+
+} // namespace Pcm
+
+#endif /* PCMMIX_H */
